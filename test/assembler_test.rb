c9655f9a7a8bdb2ef8cc5a2a4690938766abb833
Zander Mackie <zmackie@gmail.com>
2016-07-14 15:32:58 -0400

    Implement testing.

--- test/assembler_test.rb
+++ test/assembler_test.rb
@@ -1,8 +1,21 @@
 require 'minitest/autorun'
+require_relative '../hacker'
 
 describe Hacker do
-  it "parses hack assemlby files and outputs *.hack binary code" do
+  it "parses hack assembly files and outputs *.hack binary code" do
+    assembly_files = Dir['./test/fixtures/*.asm']
 
+    assembly_files.each do |file|
+      `./bin/hacksemble #{file}`
+      base, _ = File.basename(file).split('.')
+      hackfile = base + '.hack'
+
+      newfile = open(hackfile)
+      oldfile = open("./test/fixtures/proper/#{hackfile}")
+
+      newfile.readlines.must_equal oldfile.readlines
+      File.delete(hackfile)
+    end
   end
 end
 
